730db2a9c8d83f5ea4aad9365691053fadc55226
dariomilan <65947828+dariomilan@users.noreply.github.com>
2020-06-05 10:15:37 +0200

    Inserimento metodo main

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -1,9 +1,14 @@
 package com.techprimers.testing;
 
 public class FizzBuzz {
-
+public static void main(String[] args){
+        System.out.println("Hello " +  play(args[0]) );
+    }
+    
+    
+    
     public String play(int number) {
-
+ 
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
